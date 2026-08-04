6734865984064f8ecb36946fa372b8cde6c37bd3
oasisdelsur <manuelpoker2026@hotmail.com>
2026-08-04 15:32:43 +0100

    Update language.js

--- language.js
+++ language.js
@@ -1,1 +1,58 @@
+const translations = {
 
+it:{
+    dispensario:"🌿 Dispensario",
+    weed:"Weed",
+    frozen:"Frozen",
+    dry:"Dry",
+    extract:"Extract",
+    cart:"🛒 Carrello",
+    order:"📲 Conferma ordine",
+    back:"← Indietro",
+    coming:"Ancora non disponibili",
+    arrival:"Prodotti in arrivo..."
+},
+
+
+es:{
+    dispensario:"🌿 Dispensario",
+    weed:"Marihuana",
+    frozen:"Frozen",
+    dry:"Dry",
+    extract:"Extractos",
+    cart:"🛒 Carrito",
+    order:"📲 Confirmar pedido",
+    back:"← Atrás",
+    coming:"Todavía no disponibles",
+    arrival:"Productos próximamente..."
+},
+
+
+en:{
+    dispensario:"🌿 Dispensary",
+    weed:"Weed",
+    frozen:"Frozen",
+    dry:"Dry",
+    extract:"Extract",
+    cart:"🛒 Cart",
+    order:"📲 Confirm order",
+    back:"← Back",
+    coming:"Not available yet",
+    arrival:"Products coming soon..."
+},
+
+
+de:{
+    dispensario:"🌿 Shop",
+    weed:"Weed",
+    frozen:"Frozen",
+    dry:"Dry",
+    extract:"Extrakte",
+    cart:"🛒 Warenkorb",
+    order:"📲 Bestellung bestätigen",
+    back:"← Zurück",
+    coming:"Noch nicht verfügbar",
+    arrival:"Produkte kommen bald..."
+}
+
+};
